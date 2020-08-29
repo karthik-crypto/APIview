7d0b2d20a9bde732855d854d79db475ffeac77f4
Shreeda Bhat <Shreeda.bhat@yahoo.com>
2020-08-29 17:43:40 +0530

    API fixed, names fixed

--- APIVIEW/APIapp/views.py
+++ APIVIEW/APIapp/views.py
@@ -1,11 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-<<<<<<< HEAD
 from APIapp.serializers import  helloserilaizer
-=======
-from APIapp import serializers
->>>>>>> d0b074be15ea8067bf84f0f8ebb846ca94a751dd
 
 class helloAPIView(APIView):
 
@@ -17,37 +13,18 @@ class helloAPIView(APIView):
         return Response({'message':'hello','an_APIView':an_APIView})
 
 
-<<<<<<< HEAD
 class postAPIView(APIView):
     serializer_class=  helloserilaizer
     def post(self,request,format=None):
         serializer=self.serializer_class(data=request.data)
         if serializer. is_valid():
             name=serializer.validated_data.get('name')
-            message=f'hellp{name}'
-            return Response({'message':'message'})
+            message=f'hello {name}'
+            return Response({'message':message})
         else:
             return Response(
                 status=status.HTTP-400-BAD-REQUEST
             )
-=======
-        class helloAPIView(APIView):
-            serializer_class=serializers.helloserializers
-
-
-
-            def post(self,request,format=none):
-                serializer=self.serializer_class(data=request.data)
-
-                if serializer. is_valid():
-                    name=serializer.validated_data.post('name')
-                    message=f'hellp{name}'
-                    return Response({'message':'message'})
-                else:
-                    return Response(
-                    status=status.HTTP-400-BAD-REQUEST
-                    )
->>>>>>> d0b074be15ea8067bf84f0f8ebb846ca94a751dd
 
 
 
