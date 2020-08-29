0e12d58904ce3cbca228e89df4b3a11036ec845a
Shreeda Bhat <Shreeda.bhat@yahoo.com>
2020-08-29 14:41:13 +0530

    changes fixed, API working

---
+++ APIVIEW/APIapp/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from APIapp import views
+
+urlpatterns=[
+path('hello-view/',views.helloAPIView.as_view()),
+path('post',views.postAPIView.as_view()),
+]
---
+++ APIVIEW/APIapp/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from APIapp.serializers import  helloserilaizer
+
+class helloAPIView(APIView):
+
+    def get(self,request,format=None):
+        an_APIView=[
+            'uses as function(get,put,patch)'
+        ]
+
+        return Response({'message':'hello','an_APIView':an_APIView})
+
+
+class postAPIView(APIView):
+    serializer_class=  helloserilaizer
+    def post(self,request,format=None):
+        serializer=self.serializer_class(data=request.data)
+        if serializer. is_valid():
+            name=serializer.validated_data.get('name')
+            message=f'hellp{name}'
+            return Response({'message':'message'})
+        else:
+            return Response(
+                status=status.HTTP-400-BAD-REQUEST
+            )
+
+
+
