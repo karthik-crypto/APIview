05f3d934b4b395508c7fe5c50fc3eb9eb876b6aa
Shreeda Bhat <50732218+memetics19@users.noreply.github.com>
2020-08-29 17:51:23 +0530

    Update urls.py

--- APIVIEW/APIapp/urls.py
+++ APIVIEW/APIapp/urls.py
@@ -3,12 +3,6 @@ from APIapp import views
 
 urlpatterns=[
 path('hello-view/',views.helloAPIView.as_view()),
-<<<<<<< HEAD
 path('post',views.postAPIView.as_view()),
 ]
-=======
 
-path('post',views.postAPIView.as_view()),
-
-]
->>>>>>> 7c9de4e82d545c51d6bece2aacb82e437103b011
