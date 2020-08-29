81f42dcc143d089d1cf46e693be7ec967de5dd65
Shreeda Bhat <50732218+memetics19@users.noreply.github.com>
2020-08-29 14:43:53 +0530

    Update views.py

--- APIVIEW/APIapp/views.py
+++ APIVIEW/APIapp/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-<<<<<<< HEAD
+
 from APIapp.serializers import  helloserilaizer
-=======
-from APIapp import serializers
->>>>>>> d0b074be15ea8067bf84f0f8ebb846ca94a751dd
+
+
 
 class helloAPIView(APIView):
 
@@ -17,7 +16,7 @@ class helloAPIView(APIView):
         return Response({'message':'hello','an_APIView':an_APIView})
 
 
-<<<<<<< HEAD
+
 class postAPIView(APIView):
     serializer_class=  helloserilaizer
     def post(self,request,format=None):
@@ -30,24 +29,5 @@ class postAPIView(APIView):
             return Response(
                 status=status.HTTP-400-BAD-REQUEST
             )
-=======
-        class helloAPIView(APIView):
-            serializer_class=serializers.helloserializers
 
-
-
-            def post(self,request,format=none):
-                serializer=self.serializer_class(data=request.data)
-
-                if serializer. is_valid():
-                    name=serializer.validated_data.post('name')
-                    message=f'hellp{name}'
-                    return Response({'message':'message'})
-                else:
-                    return Response(
-                    status=status.HTTP-400-BAD-REQUEST
-                    )
->>>>>>> d0b074be15ea8067bf84f0f8ebb846ca94a751dd
-
-
-
+     
